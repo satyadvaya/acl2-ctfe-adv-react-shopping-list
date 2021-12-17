9000be06410606d15ae9561b75b656c2c605a86d
satyadvaya <satyadvaya@gmail.com>
2021-12-17 04:38:47 -0800

    added ItemList component

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-export default function ItemList() {
-  return <h1>Rendering ItemList</h1>;
+export default function ItemList({
+  groceries,
+  onUpdateGrocery,
+  onDeleteGrocery,
+}) {
+  return (
+    <ul>
+      {groceries.map((grocery) => (
+        <li key={grocery.id}>
+          <Item
+            grocery={grocery}
+            onUpdate={onUpdateGrocery}
+            onDelete={onDeleteGrocery}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
