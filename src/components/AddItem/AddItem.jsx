44134e8ad04cb2b603ff2907940468adc5617f07
satyadvaya <satyadvaya@gmail.com>
2021-12-17 03:16:22 -0800

    added AddItem component

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-export default function AddItem() {
-  return <h1>Rendering AddItem</h1>;
+export default function AddItem({ onAddGrocery }) {
+  const [food, setFood] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFood('');
+    onAddGrocery(food);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Enter Grocery Here"
+        value={food}
+        onChange={(event) => setFood(event.target.value)}
+      />
+      <button type="submit">
+        Add to List{' '}
+        <span role="img" aria-label="cart">
+          🛒
+        </span>
+      </button>
+    </form>
+  );
 }
