a59be36fafd5c6f49e1c52922b03d623c3639654
satyadvaya <satyadvaya@gmail.com>
2021-12-17 00:50:33 -0800

    added Reducer function to ShoppingList component

--- src/views/ShoppingList/ShoppingList.jsx
+++ src/views/ShoppingList/ShoppingList.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import AddItem from '../../components/AddItem/AddItem';
 import ItemList from '../../components/ItemList/ItemList';
 
+function groceriesReducer(groceries, action) {
+  switch (action.type) {
+    case 'added': {
+      return [...groceries, { id: action.id, food: action.food, done: false }];
+    }
+    case 'updated': {
+      return groceries.map((grocery) => {
+        if (grocery.id === action.task.id) {
+          return action.task;
+        }
+        return grocery;
+      });
+    }
+    case 'deleted': {
+      return groceries.filter((grocery) => grocery.id !== action.id);
+    }
+    default: {
+      throw Error(`Unknown action: ${action.type}`);
+    }
+  }
+}
+
 export default function ShoppingList() {
   return (
     <>
